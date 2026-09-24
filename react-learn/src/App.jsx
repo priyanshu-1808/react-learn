b96aa55e59ec12ab088a59745123406028bb696b
priyanshu-1808 <kr.priyanshu.1808@gmail.com>
2026-09-24 19:36:17 +0530

    1st commit

---
+++ react-learn/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="dad">
+      <h1 id = 'child 1'>hello</h1>
+      <h2 id = 'child 2'>hiii</h2>
+    </div>
+    <div id="chacha"></div>
+    </>
+  )
+}
+
+export default App
